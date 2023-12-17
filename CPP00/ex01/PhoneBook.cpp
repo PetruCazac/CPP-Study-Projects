a47777bd16bab6ddad2c4f58664fbea5faa818e5
pcazac <petru.cazac@yahoo.com>
2023-12-17 23:42:05 +0100

    WIP

--- CPP00/ex01/PhoneBook.cpp
+++ CPP00/ex01/PhoneBook.cpp
@@ -1,39 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*   PhoneBook.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pcazac <pcazac@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 12:44:12 by pcazac            #+#    #+#             */
-/*   Updated: 2023/12/17 13:32:50 by pcazac           ###   ########.fr       */
+/*   Updated: 2023/12/17 23:41:46 by pcazac           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-const char *maximize(char *str)
-{
-	for (int i = 0; str[i]; i++)
-	{
-		if (str[i] >= 'a' && str[i] <= 'z')
-			str[i] -= ('a' - 'A');
-	}
-	return (str);
-}
-
-int	main(int argc, char **argv)
-{
-	if (argc < 2){
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" <<std::endl;
-		return (0);
-	}
-	for (int i = 1; i < argc; i++)
-	{
-		std::cout << maximize(argv[i]);
-		if (i < argc -1)
-			std::cout << " ";
-	}
-	std ::cout << std::endl;
-	return (0);
-}
