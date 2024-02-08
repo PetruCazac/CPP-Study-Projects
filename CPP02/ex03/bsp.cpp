bbf9ed94bbd9cdf22850165a2a2c94384fe203bf
pcazac <pcazac@student.42heilbronn.de>
2024-02-08 13:38:48 +0100

    Changes to CPP02

--- CPP02/ex03/bsp.cpp
+++ CPP02/ex03/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: pcazac <pcazac@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 23:50:53 by pcazac            #+#    #+#             */
-/*   Updated: 2024/02/07 19:38:15 by pcazac           ###   ########.fr       */
+/*   Updated: 2024/02/08 13:38:21 by pcazac           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,22 @@
 #include "Fixed.hpp"
 
 float	area(Point const a, Point const b,Point const c){
-	return (((a.get_point_x() * (b.get_point_y() - c.get_point_y())
+	float f = ((a.get_point_x() * (b.get_point_y() - c.get_point_y())
 			+ b.get_point_x() * (c.get_point_y() - a.get_point_y())
-			+ c.get_point_x() * (a.get_point_y() - b.get_point_y())) / 2.0f).toFloat());
+			+ c.get_point_x() * (a.get_point_y() - b.get_point_y())) / 2.0f).toFloat();
+	if (f < 0)
+		f *= -1;
+	return f;
 }
 
 bool bsp( Point const a, Point const b, Point const c, Point const point){
 
 	Fixed triangle_area(area(a, b, c));
 	Fixed abp(area(a, b, point));
-	Fixed acp(area(a, c, point));
 	Fixed bcp(area(b, c, point));
-
+	Fixed acp(area(a, c, point));
 	bool location;
-	switch (triangle_area > (abp + acp + bcp)){
+	switch (triangle_area == (abp + acp + bcp) && abp != 0.0f && bcp != 0 && acp != 0){
 		case true :
 			location = true;
 			break;
--- CPP02/ex03/main.cpp
+++ CPP02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pcazac <pcazac@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 15:02:28 by pcazac            #+#    #+#             */
-/*   Updated: 2024/02/07 19:32:10 by pcazac           ###   ########.fr       */
+/*   Updated: 2024/02/08 13:37:50 by pcazac           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int main( void ) {
 	// Point const point(4, 4);
 	// Point const point(2, 2);
 	// Point const point(3, 3);
-	Point const point(2 , 2);
+	Point const point(3 , 4);
 	// Point const point(1, 0.1);
 
 	switch (bsp(a, b, c, point))
