9b33928d3810b74c2b0140bbb89108255b389661
pcazac <petru.cazac@yahoo.com>
2024-03-01 16:15:13 +0100

    WIP

--- CPP08/ex02/MutantStack.hpp
+++ CPP08/ex02/MutantStack.hpp
@@ -6,19 +6,23 @@
 /*   By: pcazac <pcazac@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 13:12:00 by pcazac            #+#    #+#             */
-/*   Updated: 2024/02/29 16:32:03 by pcazac           ###   ########.fr       */
+/*   Updated: 2024/03/01 16:14:08 by pcazac           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
 
-#include <iostream>
-#include <algorithm>
-#include <list>
+#include <stack>
+#include <deque>
 
+class MutantStack : public std::stack<T> {
+public:
+	typedef typename std::stack<T>::iterator it;
+	
+private:
 
-
+};
 
 #include "MutantStack.tpp"
 
